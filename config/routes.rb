76174528f412f128fb418c2a97f849c0e4bff1a0
DaniilZinoviev05 <danielzinoviev@yandex.ru>
2024-07-25 00:01:40 +0300

    design, routes and integration test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'help' => 'pages#help'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
+  get 'signup' => 'users#new'
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "pages#home"
